c820b9d1759af986d8850ff3452217d6fc25b3e6
óðinn <odinnben2000@gmail.com>
2018-02-08 13:56:11 +0000

    Create verkefni4.py

---
+++ verkefni4.py
@@ -0,0 +1,27 @@
+import os
+import json
+from bottle import route,run,template,static_file,error
+
+with open('class.json','r',encoding='utf-8') as f:
+    bekkur = json.load(f)
+
+@route('/')
+def index():
+    return template('index', bekkur=bekkur)
+
+@route('/nemandi/<kt>')
+def nemandi(kt):
+    return template('nemandi', kt=kt , bekkur=bekkur)
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root ='./static_files')
+
+@error(404)
+@error(500)
+def error404(error):
+    return '<h3>Þessi síða er ekki til.</h3>'
+
+
+
+run()
